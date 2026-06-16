f8b0786fe1f95dc5904845b7bd11565654996f04
Sagi Carmel <sagica5273@gmail.com>
2026-06-16 14:31:00 +0300

    feat: primitives almost perfect

--- src/dsl/primitives.ts
+++ src/dsl/primitives.ts
@@ -1,74 +1,116 @@
-// type String = string;
-// type Number = number;
-// type Bigint = bigint;
-// type Boolean = boolean;
-// type Undefined = undefined;
-
 import type { Trim } from "@/types.ts";
 
 // [x] - Support placing the primitives in a type as a string
 // [x] - Support placing union types of primitives: string | number | undefined
-// [ ] - Support placing union types of explicits like:
-// [ ] -  '' (string), 0 (number), true, false
-// [ ] - Support template literals ``
-// [ ] - Support union in template literals of primitives
-// [ ] - Support union in template literals of explicits
-// [ ] - Support extending the primitives dynamically
+// [x] - Support placing union types of true, false
+// [x] - Support placing union types of numbers 0 1
+// [x] - Support placing union types of strings '' / ""
+// [x] - Support template literals ``
+// [x] - Support union in template literals of primitives
+// [x] - Support union in template literals of literals
+// [x] - Support placing pipe | inside of quotes '|' / "'"
+// [ ] - Add a test: for `'"|"'` and `"'|'"`
+// [ ] - Prevent meaningless end pipe "string |"
+// [ ] - Prevent misuse of spaces "string|number" or "   string   "
+// [ ] - TODO: the type infer should receive the supported keywords as a <SK ext...>
+// [ ] - TODO: make parse
 
 // type writeUnresolvableMessage<token extends string> =
 //   `'${token}' is unresolvable`;
 
-const SUPPORTED_PRIMITIVES = [
-  "string",
-  "number",
-  "bigint",
-  "boolean",
-  "undefined",
-] as const;
+const SUPPORTED_PRIMITIVES = {
+  string: "" as string,
+  number: 0 as number,
+  bigint: BigInt(0) as bigint,
+  boolean: true as boolean,
+  undefined: undefined as undefined,
+} as const;
 
-type SupportedPrimitivesUnion = (typeof SUPPORTED_PRIMITIVES)[number];
+const SUPPORTED_LITERALS = {
+  true: true,
+  false: false,
+} as const;
+
+type SupportedKeyword = typeof SUPPORTED_PRIMITIVES & typeof SUPPORTED_LITERALS;
+
+type SupportedKeywordUnion = keyof SupportedKeyword;
 
 export type DSLString = string;
 
 type DSLValidate<
   T extends string,
-  S extends string = SupportedPrimitivesUnion,
+  S extends string = SupportedKeywordUnion,
 > = T extends `${infer L extends string}|${infer R extends string}`
-  ? Trim<L> extends S
-    ? `${Trim<L>} | ${Trim<DSLValidate<R, Exclude<S, Extract<SupportedPrimitivesUnion, Trim<L>>>>>}`
-    : [Extract<S, `${Trim<L>}${string}`>] extends [never]
-      ? `'${Trim<L>}' is not supported`
-      : `${Trim<L>} | ${Trim<DSLValidate<R>>}`
-  : Trim<T> extends S
-    ? T
-    : [Extract<S, `${Trim<T>}${string}`>] extends [never]
-      ? `'${Trim<T>}' is not supported`
-      : Extract<S, `${Trim<T>}${string}`>;
+  ? Trim<L> extends `${infer N extends number}`
+    ? `${N} | ${Trim<DSLValidate<R, S>>}`
+    : Trim<L> extends `\`${infer Str extends string}\``
+      ? Str extends `\$\{${infer innerDSL extends string}\}`
+        ? `\`${Trim<DSLValidate<innerDSL, S>>}\``
+        : Trim<L>
+      : Trim<L> extends `'${string}'` | `"${string}"`
+        ? `${Trim<L>} | ${Trim<DSLValidate<R, S>>}`
+        : Trim<L> extends S
+          ? `${Trim<L>} | ${Trim<DSLValidate<R, S>>}`
+          : [Extract<S, `${Trim<L>}${string}`>] extends [never]
+            ? `'${Trim<L>}' is not supported`
+            : `${Trim<L>} | ${Trim<DSLValidate<R, S>>}`
+  : Trim<T> extends `${infer N extends number}`
+    ? `${N}`
+    : Trim<T> extends `\`${infer Str extends string}\``
+      ? Str extends `\$\{${infer innerDSL extends string}\}`
+        ? `\`\$\{${Trim<DSLValidate<innerDSL, S>>}\}\``
+        : Trim<T>
+      : Trim<T> extends `'${string}'` | `"${string}"`
+        ? Trim<T>
+        : Trim<T> extends S
+          ? T
+          : [Extract<S, `${Trim<T>}${string}`>] extends [never]
+            ? `'${Trim<T>}' is not supported`
+            : Extract<S, `${Trim<T>}${string}`>;
 
-type InferOne<T extends string> = T extends "string"
-  ? string
-  : T extends "number"
-    ? number
-    : T extends "bigint"
-      ? bigint
-      : T extends "boolean"
-        ? boolean
-        : T extends "undefined"
-          ? undefined
-          : never;
+type InferOne<T extends string> = T extends keyof SupportedKeyword
+  ? SupportedKeyword[T]
+  : T extends `${infer N extends number}` // check if N is a number
+    ? N
+    : T extends `\`${infer S extends string}\``
+      ? S extends `\$\{${infer innerDSL extends string}\}`
+        ? `${DSLInfer<innerDSL>}`
+        : `${S}`
+      : T extends `'${infer S extends string}'` | `"${infer S extends string}"`
+        ? S
+        : never;
 
 export type DSLInfer<T extends DSLString> = T extends `${infer L}|${infer R}`
-  ? InferOne<Trim<L>> | DSLInfer<Trim<R>>
+  ? T extends `\`${infer Piped extends `${string}|${string}`}\`${infer Maybe extends string}`
+    ? Piped extends `\$\{${infer innerDSL extends string}\}`
+      ? `${DSLInfer<Trim<innerDSL>>}` | DSLInfer<Trim<Maybe>>
+      : `${Piped}`
+    : T extends `"${infer Piped extends `${string}|${string}`}"${infer Maybe extends string}`
+      ? `${Piped}` | DSLInfer<Maybe>
+      : T extends `'${infer Piped extends `${string}|${string}`}'${infer Maybe extends string}`
+        ? `${Piped}` | DSLInfer<Maybe>
+        : InferOne<Trim<L>> | DSLInfer<Trim<R>>
   : InferOne<Trim<T>>;
+
+// type X = DSLInfer<"'|' | \"|\" | `|` | `${'|'}`">;
+// type X = DSLInfer<`'"|"' | 1`>;
+
+// function isSupportedKeyword(s: string): s is SupportedKeywordUnion {
+//   return (
+//     Object.keys(SUPPORTED_PRIMITIVES).includes(s) ||
+//     Object.keys(SUPPORTED_LITERALS).includes(s)
+//   );
+// }
 
 export function dslString<const DSL extends DSLString>(
   dslString: DSLValidate<DSL>,
 ) {
-  return dslString;
-}
+  // const parts = dslString.split("|").map((p) => p.trim());
 
-function isSupportedPrimitive(s: string): s is SupportedPrimitivesUnion {
-  return SUPPORTED_PRIMITIVES.includes(s);
+  // if (parts.length === 0) {
+  //   throw new Error(`Invalid DSL string: "${dslString}"`);
+  // }
+  return dslString;
 }
 
 export function parseValueAgainstDSL<const DSL extends DSLString>(
@@ -77,21 +119,22 @@ export function parseValueAgainstDSL<const DSL extends DSLString>(
 ): DSLInfer<DSL> {
   const parts = dslString.split("|").map((p) => p.trim());
 
-  if (parts.length === 0 || !parts.every(isSupportedPrimitive)) {
-    throw new Error(`Invalid DSL string: "${dslString}"`);
-  }
-
-  const matches = parts.some((type) =>
-    type === "undefined"
-      ? checkAgainst === undefined
-      : typeof checkAgainst === type,
+  const matches = parts.some(
+    (part) =>
+      // This is for general types like string, undefined
+      typeof checkAgainst === part ||
+      // This for literals like true, false
+      (part in SUPPORTED_LITERALS &&
+        SUPPORTED_LITERALS[part as keyof typeof SUPPORTED_LITERALS] ===
+          checkAgainst) ||
+      // This is for numbers
+      (!Number.isNaN(+part) && +part === checkAgainst) ||
+      /^(('[^']*'))$|^(("[^"]*"))$|^((`[^`]*`))$/.test(part),
   );
 
   if (!matches) {
-    const actual =
-      checkAgainst === undefined ? "undefined" : typeof checkAgainst;
     throw new Error(
-      `Value of type "${actual}" does not match DSL "${dslString}"`,
+      `Value of type "${typeof checkAgainst}" does not match DSL "${dslString}"`,
     );
   }
 
