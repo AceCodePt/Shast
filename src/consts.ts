911f64ad6614b3e16116658f90b1fb2d8f81d697
Sagi Carmel <sagica5273@gmail.com>
2026-07-08 15:53:05 +0300

    chore: small modification to naming

--- src/consts.ts
+++ src/consts.ts
@@ -4,13 +4,13 @@ and let the human what you are about to do and HALT
 */
 
 import commonHTMLAttributes from "./html/attribute-config/variations/common.ts";
-import commonHTMLTag from "./html/tag-config/variations/common.ts";
+import commonHTMLTags from "./html/tag-config/variations/common.ts";
 import commonCSSSyntax from "./css/syntax-config/variations/common.ts";
 import commonCSSAttributes from "./css/attribute-config/variations/common.ts";
 import commonCSSPseudoClasses from "./css/pseudo-class-config/variations/common.ts";
 
-export const HTML_GLOBAL_ATTRIBUTES = commonHTMLAttributes;
-export const HTML_TAG_DEFINITIONS = commonHTMLTag;
-export const CSS_SYNTAX = commonCSSSyntax;
-export const CSS_ATTRIBUTES = commonCSSAttributes;
-export const CSS_GLOBAL_PSEUDO_CLASSES = commonCSSPseudoClasses;
+export const HTML_GLOBAL_ATTRIBUTES_CONFIG = commonHTMLAttributes;
+export const HTML_TAGS_CONFIG = commonHTMLTags;
+export const CSS_SYNTAX_CONFIG = commonCSSSyntax;
+export const CSS_ATTRIBUTES_CONFIG = commonCSSAttributes;
+export const CSS_GLOBAL_PSEUDO_CLASSES_CONFIG = commonCSSPseudoClasses;
